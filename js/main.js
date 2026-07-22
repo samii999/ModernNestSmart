cc0d2df8f043f2bd9d50cbbd42375faa4f1db463
samii999 <um558899@gmail.com>
2026-07-22 20:21:08 +0500

    add components

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
+// Load Header
+fetch('/components/header.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('header-placeholder').innerHTML = data;
+  });
+
+// Load Footer
+fetch('/components/footer.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('footer-placeholder').innerHTML = data;
+  });
+
+// Toggle Menu
 function toggleMenu() {
-  const links = document.querySelector('.nav-links');
-  links.classList.toggle('open');
+  const navLinks = document.querySelector('.nav-links');
+  if (navLinks) navLinks.classList.toggle('open');
 }
